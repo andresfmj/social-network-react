8b0592b46795da76051571d4fdd8f27d9cfaff74
Andres Fernando <andres.fernando08@gmail.com>
2020-08-24 12:37:39 -0500

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function App() {
+	return (
+		<div>
+			<h2>My Social Network</h2>
+			<p>where you can share photos about your pets</p>
+		</div>
+	);
+}
+
+export default App;
