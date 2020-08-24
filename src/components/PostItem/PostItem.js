4c53074c61c32d9c685511b65fcd1096410ba547
Andres Fernando <andres.fernando08@gmail.com>
2020-08-24 16:49:27 -0500

    styling PostItem component

--- src/components/PostItem/PostItem.js
+++ src/components/PostItem/PostItem.js
@@ -2,32 +2,34 @@ import React from 'react';
 
 import './PostItem.scss';
 
-function PostItem({ author, image, tags, content, likes, date, comments }) {
+function PostItem({ id, author, image, tags, content, link, likes, publishDate }) {
 
     return (
         <div className="Post-item">
             <header>
-                <h2>Author Name</h2>
+                <h2>
+                    <a href={`/user/${author.id}/post`}>{`${author.firstName} ${author.lastName}`}</a>
+                </h2>
             </header>
             <div className="Post-item-content">
-                <img className='img-rounded' src="https://via.placeholder.com/300x380.png?text=Image+Post" alt="alternative text" />
+                <img className='img-rounded' src={image} alt="" />
                 <div className="Post-item-hashtags">
                     <ul>
-                        <li>#animal</li>
-                        <li>#doglover</li>
-                        <li>#retriever</li>
+                        {
+                            tags && tags.length > 0 && tags.map((t, _i) => (
+                                <li key={_i}><a href={encodeURIComponent(`/tag/${t}/post`).replace('%20', '+')}>{`#${t}`}</a></li>
+                            ))
+                        }
                     </ul>
                 </div>
-                <p>Content for the post is showed here...</p>
+                <p>{content}</p>
             </div>
             <div className="Post-item-stats">
-                <p>(icon) 11 likes</p>
-                <p>published at (date)</p>
+                <p className='text-center'>(icon) {`${likes} likes`}</p>
+                <p className='text-center'>{`published at ${publishDate}`}</p>
             </div>
             <div className="Post-item-actions">
-                <ul>
-                    <li>Comments</li>
-                </ul>
+                <p><a href={`/post/${id}/comments`}>Show Comments</a></p>
             </div>
         </div>
     )
