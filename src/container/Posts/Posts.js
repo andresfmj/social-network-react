65ea3ea3915d7e7582a3c8ea4acfe2ae41c1ab53
Andres Fernando <andres.fernando08@gmail.com>
2020-08-24 16:51:16 -0500

    get posts list and they're shown through the PostItem component

--- src/container/Posts/Posts.js
+++ src/container/Posts/Posts.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './Posts.scss';
 import PostItem from '../../components/PostItem/PostItem';
 
+import { request } from '../../helpers/request';
+
+import { API_URL } from '../../helpers/constants';
+
+
 function Posts(props) {
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState('')
+
+    const fetchData = async () => {
+        const req = await request(`${API_URL}/post?limit=5&page=0`, 'GET', null)
+        if (req.ok) {
+            const response = await req.json()
+            const data = response.data
+            setPosts(data)
+            setLoading(false)
+        } else {
+            setError(`${req.status} ${req.statusText}`)
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
 
     return (
         <div className="Posts">
-            <PostItem />
-            <PostItem />
-            <PostItem />
-            <PostItem />
-            <PostItem />
-            
+            {loading && <p>Loading posts...</p>}
+            {error && <p>Error: {error}</p>}
+            {posts && posts.map(i => (
+                <PostItem 
+                    key={i.id} 
+                    id={i.id}
+                    author={i.owner} 
+                    image={i.image}
+                    tags={i.tags}
+                    content={i.text}
+                    link={i.link}
+                    likes={i.likes}
+                    publishDate={i.publishDate}
+                />
+            ))}
         </div>
     )
 }
