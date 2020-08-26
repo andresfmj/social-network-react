c7d05c957d0df84135d10a529473b34cf5770d36
Andres Fernando <andres.fernando08@gmail.com>
2020-08-25 22:32:35 -0500

    it asks app-id through a modal when is first time running the app

--- src/container/Posts/Posts.js
+++ src/container/Posts/Posts.js
@@ -9,6 +9,7 @@ import './Posts.scss';
 import PostItem from '../../components/PostItem/PostItem';
 
 import { request } from '../../helpers/request';
+import Modal from '../../components/Modal/Modal';
 
 
 
@@ -16,6 +17,10 @@ function Posts(props) {
     const [posts, setPosts] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState('')
+    const [toggleModal, setToggleModal] = useState(false)
+    const [token, setToken] = useState(null)
+    const [tokenText, setTokenText] = useState('')
+
     // let urlMatch = useRouteMatch()
     let urlParams = useParams()
     // const history = useHistory()
@@ -43,28 +48,59 @@ function Posts(props) {
         }
     }
 
+    const modalTokenHandler = () => {
+        setToggleModal(!toggleModal)
+    }
+
+    const updateTokenHandler = () => {
+        if (tokenText !== '') {
+            let date = new Date()
+            let token = {
+                appId: tokenText,
+                expiresIn: date.setHours(date.getHours() + 1)
+            }
+            localStorage.setItem('token_appId', JSON.stringify(token))
+            setToken(token)
+            setToggleModal(false)
+        }
+    }
+
     useEffect(() => {
-        fetchData()
-    }, [])
+        if (!localStorage.getItem('token_appId')) {
+            modalTokenHandler()
+        } else {
+            fetchData()
+            localStorage.getItem('token_appId')
+        }
+    }, [token])
 
     return (
-        <div className="Posts">
-            {loading && <p>Obteniendo publicaciones...</p>}
-            {error && <p>Error: {error}</p>}
-            {posts && posts.map(i => (
-                <PostItem 
-                    key={i.id} 
-                    id={i.id}
-                    author={i.owner} 
-                    image={i.image}
-                    tags={i.tags}
-                    content={i.text}
-                    link={i.link}
-                    likes={i.likes}
-                    publishDate={i.publishDate}
-                />
-            ))}
-        </div>
+        <React.Fragment>
+            <div className="Posts">
+                {loading && <p>Obteniendo publicaciones...</p>}
+                {error && <p>Error: {error}</p>}
+                {posts && posts.map(i => (
+                    <PostItem 
+                        key={i.id} 
+                        id={i.id}
+                        author={i.owner} 
+                        image={i.image}
+                        tags={i.tags}
+                        content={i.text}
+                        link={i.link}
+                        likes={i.likes}
+                        publishDate={i.publishDate}
+                    />
+                ))}
+            </div>
+            <Modal toggleModal={modalTokenHandler} modalIsOpen={toggleModal}>
+                <h2 className='text-center'>Token de Autenticacion</h2>
+                <div className="input-form">
+                    <input type="text" placeholder='app-id de dummyapi' value={tokenText} onChange={(e) => setTokenText(e.target.value)} />
+                    <button className='btn btn-primary' onClick={updateTokenHandler}>Guardar</button>
+                </div>
+            </Modal>
+        </React.Fragment>
     )
 }
 
--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,11 +1,16 @@
 import { TOKEN_AUTH, API_URL } from './constants';
 
 const request = async (uri, method, params) => {
+    let appId = TOKEN_AUTH
+    if (localStorage.getItem('token_appId')) {
+        let token = JSON.parse(localStorage.getItem('token_appId'))
+        appId = token.appId
+    }
     const req = await fetch(`${API_URL}${uri}`, {
         method: method,
         headers: {
             'Content-Type': 'application/json',
-            'app-id': `${TOKEN_AUTH}`
+            'app-id': `${appId}`
         },
         mode: 'cors',
         body: params ? JSON.stringify(params) : null
